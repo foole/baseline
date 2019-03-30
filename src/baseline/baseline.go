7e9e3ac7724d6fcb23a9b9ef8f5a61de5ce3f6ba
Jacob Sherwood <sport@fizzix.org>
2019-03-30 08:53:07 -0400

    Fill in FindFiles, FindArtifacts, and FindTests functions
    Also filled in RunTest function

--- src/baseline/baseline.go
+++ src/baseline/baseline.go
@@ -1,53 +1,65 @@
 package baseline
 
 import (
-    "fmt"
-    "os"
-    //"path"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	//"path"
 )
 
 
 // the basic TestResult struct.
-// baseline     path to the baseline
-// status       can be one of: passed, failed, skipped, updated, matched
+// baseline	 path to the baseline
+// status	   can be one of: passed, failed, skipped, updated, matched
 // artifactDir  directory where the artifacts from the test are written
-// stdout       path to the stdout from running test (in artifactDir)
-// stderr       path to the stderr from running test (in artifactDir)
-// diff         path to the diff of baseline and stdout (in artifactDir)
-// test         path to the test that was run
-// runtime      runtime of the test
+// stdout	   path to the stdout from running test (in artifactDir)
+// stderr	   path to the stderr from running test (in artifactDir)
+// diff		 path to the diff of baseline and stdout (in artifactDir)
+// test		 path to the test that was run
+// runtime	  runtime of the test
 type TestResult struct {
-    baseline string
-    status string
-    artifactDir string
-    stdout string
-    stderr string
-    diff string
-    test string
-    runtime string
+	baseline string
+	status string
+	artifactDir string
+	stdout string
+	stderr string
+	diff string
+	test string
+	runtime string
+}
+
+
+// Truncate the path to the test to 50 characters. The test is truncated from
+// the end of the string (since that's where the filename is)
+func TruncateTestName(test string) string {
+	if len(test) <= 50 {
+		return test
+	} else {
+		return test[len(test) - 50:len(test)]
+	}
 }
 
 // Given a path, walk it, find all the files, and return the list of all files
 // as a slice of strings
 func FindFiles(path string) []string {
-    var files []string
-    err := filepath.Walk(path func(p string i os.FileInfo, e error) error {
-        if e != nil {
-            return e
-        }
-        if !i.IsDir() {
-            files = append(files, path)
-        }
-    }
-    return files
+	var files []string
+	err := filepath.Walk(path func(p string i os.FileInfo, e error) error {
+		if e != nil {
+			return e
+		}
+		if !i.IsDir() {
+			files = append(files, path)
+		}
+	}
+	return files
 }
-
 
 // Given the root artifact directory, recurse through it and find all the
 // files in that directory. Return an array of absolute paths to the
 // artifacts.
 func FindArtifacts(dir string) []string {
-    return FindFiles(dir)
+	return FindFiles(dir)
 }
 
 // Given a list of tests/directories, find all the executable tests and return
@@ -55,26 +67,26 @@ func FindArtifacts(dir string) []string {
 // through that directory to find all the tests that match the aforementioned
 // criteria
 func FindTests(testList []string) []string {
-    var tests []string
-    var files []string
+	var tests []string
+	var files []string
 
-    // First, find all the files from all the paths in testList
-    for _, path := range testList {
-        files = append(files, FindFiles(path)...)
-    }
+	// First, find all the files from all the paths in testList
+	for _, path := range testList {
+		files = append(files, FindFiles(path)...)
+	}
 
-    // Now, select only the executable files (all tests must be executable)
-    // and add those to tests
-    for _, file := range files {
-        fs, err := os.Stat(file)
-        if err != nil {
-            panic(fmt.Sprintf("%s", err))
-        }
-        if fs.Mode() & 0555 == 0555 {
-            test = append(tests, file)
-        }
-    }
-    return tests
+	// Now, select only the executable files (all tests must be executable)
+	// and add those to tests
+	for _, file := range files {
+		fs, err := os.Stat(file)
+		if err != nil {
+			panic(fmt.Sprintf("%s", err))
+		}
+		if fs.Mode() & 0555 == 0555 {
+			test = append(tests, file)
+		}
+	}
+	return tests
 }
 
 // Shell out and run a single test. Collect STDOUT, STDERR, diff, and
@@ -86,24 +98,53 @@ func FindTests(testList []string) []string {
 //   - path to diff file
 //   - path to artifactDir
 //   - test status:
-//     - pass
-//     - fail
-//     - skipped
-//     - updated
-//     - matched
+//	 - pass
+//	 - fail
+//	 - skipped
+//	 - updated
+//	 - matched
 // Note: this test will not set pass/updated/matched and may not set fail. It
 // will only set a test status if the test is skipped or it fails due to
 // STDERR not being empty
 func (tr *TestResult) RunTest(test string, artifactDir string) {
-    panic("Not yet implemented")
+	var out bytes.Buffer
+	var err bytes.Buffer
+	testName := test
+
+	// Error handling for test
+	fs, e := os.Stat(test)
+	if e != nil {
+		panic(fmt.Sprintf("%s", err))
+	}
+	if fs.IsDir() {
+		panic("Got directory ", test, " instead of file")
+	}
+	if fs.Mode() & 0555 != 0555 {
+		panic("File ", test, " is not executable")
+	}
+
+	cmd := exec.Command(test)
+	cmd.Stdout = &out
+	cmd.Stderr = &err
+	cmd.Run()
+
+	if out.Len() != 0 {
+		e := ioutil.WriteFile(artifactDir/testName.stdout, out, 0644)
+		if e != nil {
+			panic("Unable to write file", artifactDir/testName.stdout)
+		}
+	}
+	if err.Len() != 0 {
+		e := ioutil.WriteFile(artifactDir/testName.stderr, err, 0644)
+		if e != nil {
+			panic("Unable to write file", artifactDir/testName.stderr)
+		}
+	}
 }
 
-// Truncate the path to the test to 50 characters. The test is truncated from
-// the end of the string (since that's where the filename is)
-func TruncateTestName(test string) string {
-    if len(test) <= 50 {
-        return test
-    } else {
-        return test[len(test) - 50:len(test)]
-    }
+func CreateBaseline(test string, artifactDir string) string {
+	result = RunTest(test, artifactDir)
+	baseline = test + '.baseline'
+	if result.stderr == nil {
+	}
 }
--- src/baseline/baseline_test.go
+++ src/baseline/baseline_test.go
@@ -3,30 +3,30 @@ package baseline
 import "testing"
 
 type stringPair struct {
-    value       string
-    expected    string
+	value	   string
+	expected	string
 }
 
 var StringPairs = []stringPair {
-    { "1", "1" },
-    { "short string", "short string" },
-    { "50 character string...............................",
-      "50 character string..............................." },
-    { "-51 character string...............................",
-      "51 character string..............................." },
-    { "------------------------------80 character string...............................",
-      "80 character string..............................." },
+	{ "1", "1" },
+	{ "short string", "short string" },
+	{ "50 character string...............................",
+	  "50 character string..............................." },
+	{ "-51 character string...............................",
+	  "51 character string..............................." },
+	{ "------------------------------80 character string...............................",
+	  "80 character string..............................." },
 }
 
 func TestTruncation(t *testing.T) {
-    for _, pair := range StringPairs {
-        trunc := TruncateTestName(pair.value)
-        if trunc != pair.expected {
-            t.Error(
-                "For", pair.value,
-                "Expected", pair.expected,
-                "Got", trunc,
-            )
-        }
-    }
+	for _, pair := range StringPairs {
+		trunc := TruncateTestName(pair.value)
+		if trunc != pair.expected {
+			t.Error(
+				"For", pair.value,
+				"Expected", pair.expected,
+				"Got", trunc,
+			)
+		}
+	}
 }
